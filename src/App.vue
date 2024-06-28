dad977e3bc979b2efd949b97006d6dfc1071223f
alenabauer <alena.a.bauer@gmail.com>
2024-06-29 00:19:00 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router'
+import PageHeader from '@/components/PageHeader.vue'
+import { NMessageProvider } from 'naive-ui'
 </script>
 
 <template>
-  <header>
+  <n-message-provider>
+    <page-header />
+  </n-message-provider>
+  <!-- <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
     </nav>
-  </header>
+  </header> -->
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <n-page-header subtitle="ESG Report Analyser">
+    <template #title> Verena 2.0 </template>
+    <template #avatar>
+      <n-icon size="large" color="#abba2d"> <globe /> </n-icon>
+    </template>
+  </n-page-header>
+</template>
+
+<script setup>
+import { NPageHeader, NIcon } from 'naive-ui'
+import { Globe } from '@vicons/ionicons5'
+</script>
